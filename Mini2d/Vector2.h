611f3b3db07a588de91ca04d411eddc342c6ad10
wderw <wojtekd@DESKTOP-LO0E993.localdomain>
2021-02-24 20:21:11 +0100

    Vector2 class

---
+++ Mini2d/Vector2.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <cmath>
+
+class Vector2
+{
+public:
+	explicit Vector2();
+
+	Vector2(const double, const double);
+	Vector2(const Vector2&);
+
+	Vector2& operator=(const Vector2& v);
+	Vector2& operator+=(const Vector2& other);
+	Vector2& operator-=(const Vector2& other);
+	Vector2& operator*=(const int scalar);
+
+	Vector2 operator+(const Vector2& other) const;
+	Vector2 operator-(const Vector2& other) const;
+	Vector2 operator*(const int scalar) const;
+
+	double dotProduct(const Vector2& other) const;
+	static double dotProduct(const Vector2& v1, const Vector2& v2);
+
+	double magnitude() const;
+	double squareMagnitude() const;
+	double distanceTo(const Vector2& other) const;
+	double squareDistanceTo(const Vector2& other) const;
+
+	void normalize();
+	Vector2 normalized() const;
+
+	static double angleBetween(const Vector2& v1, const Vector2& v2);
+	double angleTo(const Vector2& other) const;
+
+	double x, y;
+};
